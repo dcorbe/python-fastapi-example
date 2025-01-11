37e18de51b77d48cb93048fb09b54465e57bca61
Daniel Corbe <daniel@corbe.net>
2025-01-11 05:37:54 -0500

    There are no warnings here to suppress

--- example/__init__.py
+++ example/__init__.py
@@ -1,6 +1,7 @@
+"""Example API endpoints module."""
 from fastapi import APIRouter, Depends
 from auth.token import get_current_user
 
 router = APIRouter(prefix="/example", tags=["example"])
 
-from . import endpoints  # noqa
+from . import endpoints
